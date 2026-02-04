e66bc37389382e15b99f3d150f498cc98b01d581
Himanshu <gghimanshu333@gmail.com>
2026-02-04 11:04:31 +0530

    working on ui

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,19 +1,56 @@
 import React from 'react'
 import Header from '../components/Header'
+import BrickBG from '../assets/brickBackground.png'
+import RoomCard from '../assets/roomCard.png'
 import { useNavigate } from 'react-router-dom'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
-const Dashboard = () => {4
-    const navigate = useNavigate();
+gsap.registerPlugin(useGSAP)
 
+
+
+const Dashboard = () => {
+  
+  const navigate = useNavigate();
+
+  useGSAP(() => { 
+    gsap.to('.heading', {
+      backgroundColor : "black",
+      color : "green", 
+      borderColor : "green", 
+      duration : .5, 
+      repeat : -1,
+      ease: "sine.inOut"
+    })
+  })
   return (
-    <div>
-        <Header/>
-        <main className=' h-fit w-full p-6 flex justify-center items-center flex-wrap '>
-            <div onClick={() => { navigate('/chatrandom')}} className=' w-4/5 md:w-3/5 lg:w-2/5 h-fit py-20 px-4 m-4 flex justify-center items-center text-xl md:text-2xl lg:text-3xl cursor-pointer hover:border-4 hover-scale-1.4 hover:border-red-700 bg-red-300 rounded-sm md:rounded-lg lg:rounded-3xl'>chat random</div>
-            <div onClick={() => { navigate('/chatspecific')}} className=' w-4/5 md:w-3/5 lg:w-2/5 h-fit py-20 px-4 m-4 flex justify-center items-center text-xl md:text-2xl lg:text-3xl cursor-pointer hover:border-4 hover-scale-1.4 hover:border-pink-700 bg-pink-300 rounded-sm md:rounded-lg lg:rounded-3xl'>chat specific</div>
-            <div onClick={() => { navigate('/chatmultiple')}} className=' w-4/5 md:w-3/5 lg:w-2/5 h-fit py-20 px-4 m-4 flex justify-center items-center text-xl md:text-2xl lg:text-3xl cursor-pointer hover:border-4 hover-scale-1.4 hover:border-blue-700 bg-blue-300 rounded-sm md:rounded-lg lg:rounded-3xl'>chat multiple</div>
-            
-        </main>
+    <div className='h-[100vh] w-[100vw] relative overflow-hidden'>
+      <img src={BrickBG} className='h-full absolute w-full object-cover z-[-1000]' />
+
+      <div className='flex  justify-center p-4'>
+        <h1 className=' heading lg:text-6xl md:text-3xl text-2xl font-pixel border-2 rounded-lg px-8'>
+         CHATRETRO
+        </h1>
+
+      </div>
+
+      <div className=' h-fit w-full z-10 p-6 flex absolute justify-center items-center flex-wrap '>
+        <div onClick={() => { navigate('/chatrandom') }} className="md:[w-26rem] w-[20rem] lg:w-[30rem] md:h-[15rem] h-[10rem] lg:h-[20rem] flex justify-center items-center  m-2 relative">
+          <p className='font-pixel md:text-2xl text-xl lg:text-4xl text-amber-900'> Chat Random</p>
+          <img src={RoomCard} className='absolute z-[-100] h-full w-full' alt="" />
+          </div>
+        <div onClick={() => { navigate('/chatspecific') }} className="md:[w-26rem] w-[20rem] lg:w-[30rem] md:h-[15rem] h-[10rem] lg:h-[20rem] flex justify-center items-center  m-2 relative">
+          <p className='font-pixel md:text-2xl text-xl lg:text-4xl text-amber-900'> Chat Specific</p>
+          <img src={RoomCard} className='absolute z-[-100] h-full w-full' alt="" />
+          </div>
+        <div onClick={() => { navigate('/chatmultiple') }} className="md:[w-26rem] w-[20rem] lg:w-[30rem] md:h-[15rem] h-[10rem] lg:h-[20rem] flex justify-center items-center m-2 relative">
+          <p className='font-pixel md:text-2xl text-xl lg:text-4xl text-amber-900'> Chat Multiple</p>
+          <img src={RoomCard} className='absolute z-[-100] h-full w-full' alt="" />
+          </div>
+        
+
+      </div>
     </div>
   )
 }
